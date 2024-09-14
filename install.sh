c35231732e1e43a72aecab3edb263e9078885daa
root <root@Pranavs-MSI>
2024-09-14 16:57:11 +0530

    Added Dependencies Install Script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+
+sudo apt install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo wget \
+                   nasm mtools python3 python3-pip python3-parted scons dosfstools libguestfs-tools qemu-system-x86
+
+sudo apt-get install -y linux-image-generic
